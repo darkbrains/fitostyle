006d0c5e98a7fc771234b434ccd5b59e7301d0d8
blackdocs <aram.blackdocs@gmail.com>
2023-12-13 18:21:24 +0400

    add tests

---
+++ tests/tests.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+
+# Start fitostyle Docker container
+docker network create actions --driver bridge
+
+docker run --network actions --name fitostyle-tests -p 8080:8080 \
+  darkbrains/fitostyle:local &
+
+sleep 5
+
+
+# Test /api/healthz endpoint
+HTTP_STATUS_HEALTHZ=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/api/healthz -H "Content-Type: application/json")
+if [ "$HTTP_STATUS_HEALTHZ" -eq 200 ]; then
+  echo "/api/healthz $HTTP_STATUS_HEALTHZ OK" >> status_report.txt
+else
+  echo "/api/healthz $HTTP_STATUS_HEALTHZ Failed" >> status_report.txt
+fi
+
+sleep 5
+
+
+# Test / endpoint
+HTTP_STATUS_SLASH=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/ -H "Content-Type: application/json")
+if [ "$HTTP_STATUS_SLASH" -eq 302 ]; then
+  echo "/ $HTTP_STATUS_SLASH OK" >> status_report.txt
+else
+  echo "/ $HTTP_STATUS_SLASH Failed" >> status_report.txt
+fi
+
+sleep 5
+
+
+# Test /en endpoint
+HTTP_STATUS_EN=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/en -H "Content-Type: application/json")
+if [ "$HTTP_STATUS_EN" -eq 200 ]; then
+  echo "/en $HTTP_STATUS_EN OK" >> status_report.txt
+else
+  echo "/en $HTTP_STATUS_EN Failed" >> status_report.txt
+fi
+
+sleep 5
+
+
+# # Test /ru endpoint
+# HTTP_STATUS_RU=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/ru -H "Content-Type: application/json")
+# if [ "$HTTP_STATUS_RU" -eq 200 ]; then
+#   echo "/ru $HTTP_STATUS_RU OK" >> status_report.txt
+# else
+#   echo "/ru $HTTP_STATUS_RU Failed" >> status_report.txt
+# fi
+
+# sleep 5
+
+
+# # Test /am endpoint
+# HTTP_STATUS_AM=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/am -H "Content-Type: application/json")
+# if [ "$HTTP_STATUS_AM" -eq 200 ]; then
+#   echo "/am $HTTP_STATUS_AM OK" >> status_report.txt
+# else
+#   echo "/am $HTTP_STATUS_AM Failed" >> status_report.txt
+# fi
+
+# sleep 5
+
+
+# # Test /hotels endpoint
+# HTTP_STATUS_SLASH=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/hotels -H "Content-Type: application/json")
+# if [ "$HTTP_STATUS_SLASH" -eq 302 ]; then
+#   echo "/hotels $HTTP_STATUS_SLASH OK" >> status_report.txt
+# else
+#   echo "/hotels $HTTP_STATUS_SLASH Failed" >> status_report.txt
+# fi
+
+# sleep 5
+
+
+# # Test /hotels/en endpoint
+# HTTP_STATUS_HOTELS_EN=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/hotels/en -H "Content-Type: application/json")
+# if [ "$HTTP_STATUS_HOTELS_EN" -eq 200 ]; then
+#   echo "/hotels/en $HTTP_STATUS_HOTELS_EN OK" >> status_report.txt
+# else
+#   echo "/hotels/en $HTTP_STATUS_HOTELS_EN Failed" >> status_report.txt
+# fi
+
+# sleep 5
+
+
+# # Test /hotels/ru endpoint
+# HTTP_STATUS_HOTELS_RU=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/hotels/ru -H "Content-Type: application/json")
+# if [ "$HTTP_STATUS_HOTELS_RU" -eq 200 ]; then
+#   echo "/hotels/ru $HTTP_STATUS_HOTELS_RU OK" >> status_report.txt
+# else
+#   echo "/hotels/ru $HTTP_STATUS_HOTELS_RU Failed" >> status_report.txt
+# fi
+
+# sleep 5
+
+
+# # Test /hotels/am endpoint
+# HTTP_STATUS_HOTELS_AM=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/hotels/am -H "Content-Type: application/json")
+# if [ "$HTTP_STATUS_HOTELS_AM" -eq 200 ]; then
+#   echo "/hotels/am $HTTP_STATUS_HOTELS_AM OK" >> status_report.txt
+# else
+#   echo "/hotels/am $HTTP_STATUS_HOTELS_AM Failed" >> status_report.txt
+# fi
+
+# sleep 5
+
+
+# Test /empty
+HTTP_STATUS_EMPTY=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/empty -H "Content-Type: application/json")
+if [ "$HTTP_STATUS_EMPTY" -eq 404 ]; then
+  echo "/empty $HTTP_STATUS_EMPTY OK" >> status_report.txt
+else
+  echo "/empty $HTTP_STATUS_EMPTY Failed" >> status_report.txt
+fi
+
+sleep 5
+
+
+# Stop and remove the Docker containers
+docker stop fitostyle-tests
+docker rm fitostyle-tests
+
+
+# Status Report
+if grep -iwq "Failed" status_report.txt; then
+  echo "Tests Failed"
+  grep -iw "Failed" status_report.txt
+  exit 1
+else
+  echo "Tests Passed"
+  grep -iw "OK" status_report.txt
+fi
